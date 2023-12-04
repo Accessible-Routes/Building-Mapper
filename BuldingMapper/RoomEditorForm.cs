3a6c84e39ebce6bdd90d3e41c1cb2972e27af9d2
Toggytokyo <42394145+Toggytokyo@users.noreply.github.com>
2023-12-03 23:41:30 -0500

    Updated RoomEditorForm to use a new method of tracking changes made to a room. Should be more flexible to users making many changes in nested RoomEditorForms.

--- BuldingMapper/RoomEditorForm.cs
+++ BuldingMapper/RoomEditorForm.cs
@@ -10,20 +10,35 @@ using System.Windows.Forms;
 
 namespace BuildingMapper
 {
+    public enum ChangeType
+    {
+        Add,
+        Edit,
+        Remove
+    }
+
+    public struct RoomChange
+    {
+        public ChangeType ChangeType;
+        public string Target;
+        public Room? NewRoom;
+    }
+
     public class RoomEditorFormResult : FormResult
     {
-        public List<Room> Rooms { get; set; }
+        public List<RoomChange> Changes { get; set; }
 
         public RoomEditorFormResult()
         {
-            Rooms = new List<Room>();
+            Changes = new List<RoomChange>();
         }
     }
 
     public partial class RoomEditorForm : Form
     {
-        List<Room> newRooms;
+        List<RoomChange> newChanges;
         List<Room> existingRooms;
+        Room? RoomBackup;
 
         public RoomEditorForm(List<Room> existingRooms)
         {
@@ -65,7 +80,7 @@ namespace BuildingMapper
 
             List<string> roomNames = new List<string>();
 
-            //Add each AccesibilityTag enum to a list as a string
+            //Add each Room Name to a list as a string
             foreach (Room r in existingRooms)
             {
                 roomNames.Add(r.Name);
@@ -79,50 +94,68 @@ namespace BuildingMapper
                 connectionsCheckedListBox.Items.Add(name);
             }
 
-            newRooms = new List<Room>();
+            newChanges = new List<RoomChange>();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            UpdateControls();
+            UpdateSaveButton();
         }
 
-        public RoomEditorFormResult ShowRoomEditor()
+        public RoomEditorFormResult ShowAddRoomEditor()
         {
             DialogResult dialogResult = ShowDialog();
-            RoomEditorFormResult result;
 
-            if (dialogResult == DialogResult.OK)
-            {
-                Room room = new Room(roomNameTextBox.Text);
-                RoomType type = RoomType.Unspecified;
-                Enum.TryParse<RoomType>(roomTypeComboBox.SelectedItem.ToString(), true, out type);
-                room.Type = type;
-                newRooms.Add(room);
-                result = new RoomEditorFormResult() 
-                { 
-                    DialogResult = dialogResult, 
-                    Rooms = newRooms 
-                };
-            }
-            else
-            {
-                result = new RoomEditorFormResult()
-                {
-                    DialogResult = dialogResult,
-                    Rooms = new List<Room>()
-                };
-            }
-
+            RoomEditorFormResult result = GetFormResult(dialogResult, ChangeType.Add);
+            
             return result;
+        }
+
+        private RoomEditorFormResult GetFormResult(DialogResult dialogResult, ChangeType changeType)
+        {
+            RoomType type;
+            
+            if(roomTypeComboBox.SelectedItem == null || !Enum.TryParse<RoomType>(roomTypeComboBox.SelectedItem.ToString(), true, out type))
+            {
+                type = RoomType.Unspecified;
+            }
+
+            //TODO: Parse selected connected rooms
+
+            //TODO: Parse selected accessibility tags
+
+            //Create our new Room object w/ data entered
+            Room newRoom = new Room()
+            {
+                Name = roomNameTextBox.Text,
+                Type = type,
+            };
+
+            //Create our RoomChange object w/ necessary data
+            RoomChange newChange = new RoomChange()
+            {
+                ChangeType = changeType,
+                Target = newRoom.Name,
+                NewRoom = newRoom
+            };
+
+            newChanges.Add(newChange);
+
+            RoomEditorFormResult formResult = new RoomEditorFormResult()
+            {
+                DialogResult = dialogResult,
+                Changes = newChanges
+            };
+
+            return formResult;
         }
 
         private void roomNameTextBox_TextChanged(object sender, EventArgs e)
         {
-            UpdateControls();
+            UpdateSaveButton();
         }
 
-        private void UpdateControls()
+        private void UpdateSaveButton()
         {
             saveButton.Enabled = roomNameTextBox.Text != "" && roomTypeComboBox.SelectedItem != null;
         }
@@ -141,32 +174,87 @@ namespace BuildingMapper
 
         private void addButton_Click(object sender, EventArgs e)
         {
-            List<Room> combinedRooms = new List<Room>();
-            combinedRooms.AddRange(existingRooms);
-            combinedRooms.AddRange(newRooms);
+            List<Room> combinedRooms = GetCombinedRooms(existingRooms, newChanges);
 
             RoomEditorForm newForm = new RoomEditorForm(combinedRooms);
-            RoomEditorFormResult result = newForm.ShowRoomEditor();
+            RoomEditorFormResult result = newForm.ShowAddRoomEditor();
 
             if (result.DialogResult == DialogResult.OK)
             {
-                newRooms.AddRange(result.Rooms);
+                newChanges.AddRange(result.Changes);
             }
 
             UpdateRoomList();
         }
 
-        private void UpdateRoomList()
+        private List<Room> GetCombinedRooms(List<Room> rooms, List<RoomChange> changes)
         {
             List<Room> combinedRooms = new List<Room>();
-            combinedRooms.AddRange(existingRooms);
+            //We don't want to see any rooms that we've removed, so dont send
+            //those to the new form
+            //Our rooms list may contain rooms we've edited. If a room was edited,
+            //we want to take the updated data from the appropriate RoomChange 
+            //object instead.
+            //TODO: This is n^2 efficiency... could be better
+            foreach (Room room in rooms)
+            {
+                bool shouldBeAdded = true;
+                foreach (RoomChange change in newChanges)
+                {
+                    if (change.ChangeType == ChangeType.Remove)
+                    {
+                        if (change.Target == room.Name)
+                        {
+                            shouldBeAdded = false;
+                            break;
+                        }
+                    }
+
+                    else if (change.ChangeType == ChangeType.Edit)
+                    {
+                         if (change.Target == room.Name)
+                         {
+                            shouldBeAdded = false;
+                            break;
+                         }
+                    }
+                }
+
+                if (shouldBeAdded)
+                {
+                    combinedRooms.Add(room);
+                }
+            }
+
+            //We want to see any new rooms we've created, so we need to 
+            //grab them from our change list.
+            //We also grab any rooms that may have been edited.
+            List<Room> newRooms = new List<Room>();
+            foreach (RoomChange change in changes)
+            {
+                if (change.NewRoom != null && change.ChangeType == ChangeType.Add)
+                {
+                    newRooms.Add(change.NewRoom);
+                }
+                else if (change.NewRoom != null && change.ChangeType == ChangeType.Edit)
+                {
+                    newRooms.Add(change.NewRoom);
+                }
+            }
             combinedRooms.AddRange(newRooms);
+
+            return combinedRooms;
+        }
+
+        private void UpdateRoomList()
+        {
+            List<Room> combinedRooms = GetCombinedRooms(existingRooms, newChanges);
 
             connectionsCheckedListBox.Items.Clear();
 
             List<string> roomNames = new List<string>();
 
-            //Add each AccesibilityTag enum to a list as a string
+            //Add each room name to a list as a string
             foreach (Room r in combinedRooms)
             {
                 roomNames.Add(r.Name);
@@ -174,7 +262,7 @@ namespace BuildingMapper
 
             roomNames.Sort();
 
-            //Add the sorted AccesibilityTag list into the comboBox
+            //Add the sorted room names list into the comboBox
             foreach (string name in roomNames)
             {
                 connectionsCheckedListBox.Items.Add(name);
@@ -183,15 +271,15 @@ namespace BuildingMapper
             string s = (string)connectionsCheckedListBox.SelectedItem;
         }
 
-        private bool IsInList(string roomName)
-        {
-            foreach (Room r in newRooms)
-            {
-                if (r.Name == roomName) return true;
-            }
+        //private bool IsInList(string roomName)
+        //{
+        //    foreach (Room r in newRooms)
+        //    {
+        //        if (r.Name == roomName) return true;
+        //    }
 
-            return false;
-        }
+        //    return false;
+        //}
 
         private void editButton_Click(object sender, EventArgs e)
         {
